60d3101718ef768bb45d4c95ee14e9c2d0c49cfc
Akanimo <Akanumoh4@gmail.com>
2021-12-09 10:28:36 +0100

    Comment

--- dict.py
+++ dict.py
@@ -2,6 +2,7 @@ dictionary = {"dict 1" : {"name" : "akanimo", "surname" : "umoh"},
 "dict 2" : {"country" : "nigeria", "state" : "akwa ibom"},
 "dict 3" : {"city" : "uyo", "tribe" : "ibibio"}}
 for key, value in dictionary.items():
+    """Accessing the items"""
     print (f"\n{key}")
     for key in value.keys():
         print (key)
