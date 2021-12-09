15016999521e8fde92a57b4cc7b1915939cf64cb
Akanimo <Akanumoh4@gmail.com>
2021-12-09 10:26:09 +0100

    Accessing Nested Dictionary

---
+++ dict.py
@@ -0,0 +1,9 @@
+dictionary = {"dict 1" : {"name" : "akanimo", "surname" : "umoh"},
+"dict 2" : {"country" : "nigeria", "state" : "akwa ibom"},
+"dict 3" : {"city" : "uyo", "tribe" : "ibibio"}}
+for key, value in dictionary.items():
+    print (f"\n{key}")
+    for key in value.keys():
+        print (key)
+    for value in value.values():
+        print (value)
